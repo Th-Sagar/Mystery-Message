04880844a483d49a5bcc1399e3640c1fa4a7383e
Th-Sagar <sagarthapa98122@gmail.com>
2024-09-14 13:04:55 +0545

    feat: react form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,36 +1,7 @@
-"use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import Link from "next/link";
-import { useDebounceValue } from "usehooks-ts";
-import { useState } from "react";
-import { useToast } from "@/components/hooks/use-toast";
-import { useRouter } from "next/navigation";
-import { signInSchema } from "@/schemas/sigInSchema";
+import React from "react";
 
 const page = () => {
-  const [username, setUsername] = useState("");
-  const [usernameMessage, setUsernameMessage] = useState("");
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const debouncedUsername = useDebounceValue(username, 300);
-  const { toast } = useToast();
-  const router = useRouter();
-
-  //zod implementation
-
-  const form = useForm({
-    resolver: zodResolver(signInSchema),
-    defaultValues: {
-      username: "",
-      email: "",
-      password: "",
-    },
-  });
-
-  return <div></div>;
+  return <div>page</div>;
 };
 
 export default page;
