a27fe5f66dbbf57543f58257df9d69d7c9d4545a
Th-Sagar <sagarthapa98122@gmail.com>
2024-09-15 15:35:06 +0545

    feat: delete api

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -23,6 +23,8 @@ import { Button } from "./ui/button";
 import { X } from "lucide-react";
 import { Message } from "@/model/User";
 import { useToast } from "@/hooks/use-toast";
+import axios from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
 
 type MessageCardProps = {
   message: Message;
@@ -30,7 +32,17 @@ type MessageCardProps = {
 };
 const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
   const { toast } = useToast();
-  const handleDeleteConfirm = () => {};
+  const handleDeleteConfirm = async () => {
+    const response = await axios.delete<ApiResponse>(
+      `/api/delete-message/${message._id}`
+    );
+
+    toast({
+      title: response.data.message,
+    });
+
+    onMessageDelete(message._id);
+  };
 
   return (
     <Card>
